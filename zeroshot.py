260ac4924a7c82b859ab52001766906553a81a0a
Julien Dirani <juliendirani@gmail.com>
2021-12-09 17:20:36 -0500

    Update zeroshot.py

--- zeroshot.py
+++ zeroshot.py
@@ -14,60 +14,10 @@ import numpy as np
 import os
 
 
-def zeroshot_classification(X, y, clf):
-    ''''
-    Zero-shot classification.
-    Looping over time dimension not implemented within function.
-
-    PIPELINE:
-    Leave-2-out split:
-        Train classifier
-        Predict 2 vectors of leave2out
-        Compare predicted vectors (p1, p2) to model vectors (s1,s2) using cosine distance:
-            IF  distance(p1,s1) + distance(p2,s2) < distance(p1,s2) + distance(p2,s1)
-                    HIT
-            ELSE
-                    MISS
-
-    Repeat for all possible pairs
+# ========================= simple zeroshot classifier ======================= #
+def _leave2out(idx_leave2out, X, y, clf):
     '''
-
-
-    n_trials = X.shape[0]
-    indices_pairs = list(combinations( range(n_trials), 2))
-
-    hits = []
-    for idx_leave2out in tqdm(indices_pairs): # leave 2 out loop, all possible pairs
-
-        # Train-test-split using leave2out
-        X_train = np.delete(X, idx_leave2out, axis=0)
-        X_test = X[idx_leave2out,:]
-
-        y_train = np.delete(y, idx_leave2out, axis=0)
-        y_test = y[idx_leave2out,:]
-
-        # fit classifier on training data
-        clf.fit(X_train, y_train)
-
-        # predicter 2 left out
-        preds = clf.predict(X_test)
-
-        hit = cosine(preds[0],y_test[0]) + cosine(preds[1],y_test[1]) < cosine(preds[0],y_test[1]) + cosine(preds[1],y_test[0])
-
-        if hit:
-            hits.append(1)
-        else:
-            hits.append(0)
-
-    accuracy = sum(hits) / len(hits)
-
-    return accuracy
-
-
-
-def _leave2out(idx_leave2out, X, y):
-    '''
-    Called by zeroshot_classification_parallelJobs.
+    Called by zeroshot_classification
 
     '''
 
@@ -92,23 +42,70 @@ def _leave2out(idx_leave2out, X, y):
         return 0
 
 
-def zeroshot_classification_parallelJobs(X, y, clf, n_jobs):
+def zeroshot_classification(X, y, clf, n_jobs):
     ''''
-    Same as zeroshot_classification but 5-6x faster.
+    Same as zeroshot_classification() but 5-6x faster.
+
+    Zero-shot classification.
+    Looping over time dimension not implemented within function. X,y are data
+    at time slices with shape [n_trials, n_sensors]
+
+    PIPELINE:
+    Leave-2-out split:
+        Train classifier
+        Predict 2 vectors of leave2out
+        Compare predicted vectors (p1, p2) to model vectors (s1,s2) using cosine distance:
+            IF  distance(p1,s1) + distance(p2,s2) < distance(p1,s2) + distance(p2,s1)
+                    HIT
+            ELSE
+                    MISS
+
+    Repeat for all possible pairs
+
     '''
 
 
     n_trials = X.shape[0]
     indices_pairs = list(combinations( range(n_trials), 2))
 
-    hits =  Parallel(n_jobs=n_jobs)(delayed(_leave2out)(i, X, y)  for i in indices_pairs)
+    hits = Parallel(n_jobs=n_jobs)(delayed(_leave2out)(i, X, y, clf)  for i in indices_pairs)
     accuracy = sum(hits) / len(hits)
 
     return accuracy
 
 
 
-def zeroshot_classification_condGen(X_train, X_test, y_train, y_test):
+# ============= Condition generalization zeroshot classifier ================== #
+# Key differnce:
+# Clf is trained only once on the full training dataset
+# ==> _leave2out_condGen() does not include clf.fit()
+# ==> _leave2out_condGen() : No selection of traning data since clf already fit
+#       on full traning dataset
+
+def _leave2out_condGen(idx_leave2out, X_test, y_test, clf):
+    '''
+    zeroshot_classification_condGen
+    clf already fit to traning data, see zeroshot_classification_condGen
+
+    '''
+
+    # Get testing pairs from idx_leave2out. Training data is the whole traning dataset of the traning condition
+    X_test_pair = X_test[idx_leave2out,:] # current test pairs of X_test
+    y_test_pair = y_test[idx_leave2out,:] # current test pairs of y_test
+
+    # predict 2 left out
+    preds = clf.predict(X_test_pair)
+
+    hit = cosine(preds[0],y_test_pair[0]) + cosine(preds[1],y_test_pair[1]) < cosine(preds[0],y_test_pair[1]) + cosine(preds[1],y_test_pair[0])
+
+    if hit:
+        return 1
+    else:
+        return 0
+
+
+
+def zeroshot_classification_condGen(X_train, X_test, y_train, y_test, clf, n_jobs):
 
     ''''
     Zero-shot classification with condition generalization.
@@ -128,29 +125,85 @@ def zeroshot_classification_condGen(X_train, X_test, y_train, y_test):
     Repeat for all possible pairs
     Get accuracy accross all pairs
     '''
-    # fit classifier on training data
+    # fit classifier on training dataset, only once
     clf.fit(X_train, y_train)
 
     # create list of indices of all possible pairs
     n_trials = X_train.shape[0]
     indices_pairs = list(combinations( range(n_trials), 2))
 
-    hits = []
-    for idx_pairs in tqdm(indices_pairs): # loop through all possible pairs
-
-        X_tested_pairs = X[idx_pairs,:]
-        y_tested_pairs = y_test[idx_pairs,:]
-
-        # predicter 2 left out
-        preds = clf.predict(X_tested_pairs)
-
-        hit = cosine(preds[0],y_tested_pairs[0]) + cosine(preds[1],y_tested_pairs[1]) < cosine(preds[0],y_tested_pairs[1]) + cosine(preds[1],y_tested_pairs[0])
-
-        if hit:
-            hits.append(1)
-        else:
-            hits.append(0)
+    hits = Parallel(n_jobs=n_jobs)(delayed(_leave2out_condGen)(i, X_test, y_test, clf)  for i in indices_pairs)
 
     accuracy = sum(hits) / len(hits)
 
     return accuracy
+
+
+
+# class zeroshot_condGen():
+#     ''''
+#     Zero-shot classification with generalization accross condition.
+#     Looping over time dimension not implemented within function. X,y are data
+#     at time slices with shape [n_trials, n_sensors]
+#
+#     PIPELINE:
+#
+#     Train classifier on training condition
+#     For all possible pairs of testing condition
+#         Predict vectors
+#         Compare predicted vectors (p1, p2) to model vectors (s1,s2) using cosine distance:
+#             IF  distance(p1,s1) + distance(p2,s2) < distance(p1,s2) + distance(p2,s1)
+#                     HIT
+#             ELSE
+#                     MISS
+#
+#     Repeat for all possible pairs
+#     Get accuracy accross all pairs
+#
+#
+#     '''
+#
+#
+#     def __init__(self, X_train, X_test, y_train, y_test, clf):
+#         self.X_train = X_train
+#         self.X_test = X_test
+#         self.y_train = y_train
+#         self.y_test = y_test
+#         self.clf = clf
+#
+#
+#     def fit(self):
+#         clf.fit(self.X_train, self.y_train)
+#
+#     def _leave2out_condGen(self, idx_leave2out):
+#         '''
+#         zeroshot_classification_condGen
+#         clf already fit to traning data, see zeroshot_classification_condGen
+#
+#         '''
+#
+#         # Get testing pairs from idx_leave2out. Training data is the whole traning dataset of the traning condition
+#         X_test_pair = self.X_test[idx_leave2out,:] # current test pairs of X_test
+#         y_test_pair = self.y_test[idx_leave2out,:] # current test pairs of y_test
+#
+#         # predict 2 left out
+#         preds = clf.predict(X_test_pair)
+#
+#         hit = cosine(preds[0],y_test_pair[0]) + cosine(preds[1],y_test_pair[1]) < cosine(preds[0],y_test_pair[1]) + cosine(preds[1],y_test_pair[0])
+#
+#         if hit:
+#             return 1
+#         else:
+#             return 0
+#
+#
+#     def score(self, n_jobs):
+#
+#         n_trials = self.X_test.shape[0]
+#         indices_pairs = list(combinations( range(n_trials), 2))
+#
+#         hits =  Parallel(n_jobs=n_jobs)(delayed(self._leave2out)(i)  for i in indices_pairs)
+#         accuracy = sum(hits) / len(hits)
+#
+#         return accuracy
+
